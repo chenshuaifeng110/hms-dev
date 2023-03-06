4e90b82a7291744a7853fae9d3e0e70b3755a36b
“chenshuaifeng110” <997174616@qq.com>
2023-03-06 13:52:16 +0800

    feat:git hooks

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/about">About1</RouterLink>
       </nav>
     </div>
   </header>
